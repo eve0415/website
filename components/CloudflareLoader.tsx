44a47f7e82a83418a648b88b119972d993c2669d
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-12 00:43:59 +0900

    Ok, I'm sleepy definetly

--- components/CloudflareLoader.tsx
+++ components/CloudflareLoader.tsx
@@ -3,7 +3,7 @@ import type { ImageLoaderProps, ImageProps } from 'next/image';
 import Image from 'next/image';
 
 const cloudflareLoader = ({ src, width, quality }: ImageLoaderProps) =>
-    `/api/images?width=${width}&quality=${quality ?? 75}&${process.env.CF_PAGES_URL}image=${src.substring(
+    `/api/images?width=${width}&quality=${quality ?? 75}&image=${process.env.CF_PAGES_URL}${src.substring(
         1
     )}`;
 
