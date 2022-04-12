8d4e1cf0814b5d5095c1cbe111fc153de33e6595
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-12 10:18:18 +0900

    feat(ogp): Add OGP (#4)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,13 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 export default class WebDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang='ja'>
                 <Head>
                     <link rel='dns-prefetch' href='//fonts.googleapis.com' />
                     <link rel='dns-prefetch' href='//fonts.gtatic.com/' />
 
-                    <link rel='preconnect' href='https://fonts.googleapis.com' />
-                    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
+                    <link rel='preconnect' href='https://fonts.googleapis.com' crossOrigin='anonymous' />
+                    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
 
                     <link
                         rel='stylesheet'
@@ -18,6 +18,22 @@ export default class WebDocument extends Document {
 
                     <link rel='apple-touch-icon' type='image/png' href='/apple-touch-icon-180x180.png' />
                     <link rel='icon' type='image/png' href='/icon-192x192.png' />
+
+                    <meta charSet='utf-8' />
+
+                    <meta property='og:title' content='eve0415' />
+                    <meta property='og:description' content='eve0415 のホームページ' />
+                    <meta property='og:url' content='https://eve0415.net' />
+                    <meta
+                        property='og:image'
+                        content='https://eve0415.net/api/images?quality=75&image=https://eve0415.net/icon-192x192.png'
+                    />
+                    <meta property='og:type' content='website' />
+                    <meta property='og:locale' content='ja_JP' />
+
+                    <meta name='twitter:site' content='@eveevekun' />
+                    <meta name='twitter:creator' content='@eveevekun' />
+                    <meta name='twitter:card' content='summary' />
                 </Head>
                 <body style={{ backgroundColor: '#E5FCFB' }}>
                     <Main />
