e1bb8631688bb9a1547401b54dc89857ec2d4c07
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-12 16:06:53 +0900

    For testing

--- components/CloudflareLoader.tsx
+++ components/CloudflareLoader.tsx
@@ -3,7 +3,7 @@ import type { ImageLoaderProps, ImageProps } from 'next/image';
 import Image from 'next/image';
 
 const cloudflareLoader = ({ src, width, quality }: ImageLoaderProps) =>
-    `https://eve0415.net/api/images?width=${width}&quality=${quality ?? 75}&image=${src}`;
+    `https://images.eve0415.workers.dev/?width=${width}&quality=${quality ?? 75}&image=${src}`;
 
 export function CloudflareImage(props: ImageProps) {
     if (process.env.NODE_ENV === 'development') {
