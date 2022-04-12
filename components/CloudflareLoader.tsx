b4cc65ab1a005327fcb3d7196b811e4bd14d6bf7
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-12 09:22:10 +0900

    fix(image): Fix image not loading (#2)

--- components/CloudflareLoader.tsx
+++ components/CloudflareLoader.tsx
@@ -3,14 +3,12 @@ import type { ImageLoaderProps, ImageProps } from 'next/image';
 import Image from 'next/image';
 
 const cloudflareLoader = ({ src, width, quality }: ImageLoaderProps) =>
-    `https://images.eve0415.workers.dev?width=${width}&quality=${
-        quality ?? 75
-    }&image=https://eve0415.net${src}`;
+    `/api/images?width=${width}&quality=${quality ?? 75}&image=${src}`;
 
 export function CloudflareImage(props: ImageProps) {
     if (process.env.NODE_ENV === 'development') {
         return <Image unoptimized={true} {...props} />;
     } else {
-        return <Image {...props} loader={cloudflareLoader} />;
+        return <Image {...props} loader={cloudflareLoader} referrerPolicy='origin' />;
     }
 }
