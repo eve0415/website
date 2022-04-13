e02f9755475a770d1ad5bb8338622fe340b6bd51
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-13 10:31:24 +0900

    Add priority

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,6 +50,7 @@ export default function Head() {
                                 layout='fill'
                                 placeholder='blur'
                                 sizes='(min-width: 1200px) 300px, (min-width: 900px) 200px, 100px'
+                                priority
                             />
                         </Avatar>
 
