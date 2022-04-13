244dc1c48616f096e75e5d029afacd417c035218
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-13 10:52:49 +0900

    Add config just in case

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,15 @@ const nextConfig = {
         loader: 'custom',
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 286, 300, 384, 600],
         formats: ['image/avif', 'image/webp'],
-        domains: ['cdn.buymeacoffee.com', 'cdn.ko-fi.com']
+        domains: [
+            'opengraph.githubassets.com',
+            'raw.githubusercontent.com',
+            'repository-images.githubusercontent.com',
+            's3.amazonaws.com',
+            'getsileo.app',
+            'cdn.buymeacoffee.com',
+            'cdn.ko-fi.com'
+        ]
     },
 };
 
