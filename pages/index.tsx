6e8ae00d59fff2446af85ec826052d70de5c5ee9
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-16 13:58:10 +0900

    chore(Home): Change display order (#16)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import { Avatar, Box, NoSsr, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, NoSsr, useMediaQuery, useTheme } from '@mui/material';
+import dynamic from 'next/dynamic';
 import { CloudflareImage, Main } from '../components';
 import cat from '../public/images/cat.jpg';
 import catSleeping from '../public/images/catSleeping.jpg';
-import profilePic from '../public/images/me.png';
+
+const Home = dynamic(() => import('../components/Home'));
 
 export default function Head() {
     return (
@@ -30,40 +32,7 @@ export default function Head() {
 
             <Box position='fixed' width='100vw' height='100vh' bgcolor='rgba(0,0,0,0.5)'>
                 <Main>
-                    <Box
-                        display='flex'
-                        justifyContent='center'
-                        flexDirection='column'
-                        alignItems='center'
-                        height={`calc(100vh - ${useTheme().spacing(10)})`}
-                        pb={{ xs: '20%', md: 0 }}
-                        color='white'
-                    >
-                        <Avatar
-                            sx={{
-                                width: { xs: 100, md: 200, lg: 300 },
-                                height: { xs: 100, md: 200, lg: 300 },
-                            }}
-                        >
-                            <CloudflareImage
-                                src={profilePic}
-                                alt='My profile picture'
-                                layout='fill'
-                                placeholder='blur'
-                                sizes='(min-width: 1200px) 300px, (min-width: 900px) 200px, 100px'
-                                priority
-                            />
-                        </Avatar>
-
-                        <Typography variant='h3' pt={4}>
-                            eve0415
-                        </Typography>
-
-                        <Typography variant='body1' pt={1}>
-                            ただの大学生
-                        </Typography>
-                        <Typography variant='body1'>いつも眠たい人</Typography>
-                    </Box>
+                    <Home />
                 </Main>
             </Box>
         </>
