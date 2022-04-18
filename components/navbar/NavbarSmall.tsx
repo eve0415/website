ddd621fe59d6916f8fc32692a60c79fcf157eee8
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-18 10:07:23 +0900

    fix(drawer): Button being invisible

--- components/navbar/NavbarSmall.tsx
+++ components/navbar/NavbarSmall.tsx
@@ -14,27 +14,24 @@ export default function NavbarSmall({
     const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
     return (
-        <Box>
-            <IconButton
-                size='large'
-                aria-label='Menu'
-                onClick={() => (isOpen ? close() : open())}
-                sx={{
-                    backgroundColor: '#E5FCFB',
-                    position: 'fixed',
-                    borderRadius: '50%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    height: 100,
-                    width: 100,
-                    top: -20,
-                    left: -20,
-                    zIndex: 1500,
-                }}
+        <>
+            <Box
+                sx={{ backgroundColor: '#E5FCFB' }}
+                position='fixed'
+                height={100}
+                width={100}
+                left={-20}
+                top={-20}
+                borderRadius='50%'
+                display='flex'
+                alignItems='center'
+                justifyContent='center'
+                zIndex={1500}
             >
-                {isOpen ? <Close sx={{ fontSize: '40px' }} /> : <Menu sx={{ fontSize: '40px' }} />}
-            </IconButton>
+                <IconButton size='large' aria-label='Menu' onClick={() => (isOpen ? close() : open())}>
+                    {isOpen ? <Close sx={{ fontSize: '40px' }} /> : <Menu sx={{ fontSize: '40px' }} />}
+                </IconButton>
+            </Box>
 
             <SwipeableDrawer
                 open={isOpen}
@@ -45,6 +42,6 @@ export default function NavbarSmall({
             >
                 {children}
             </SwipeableDrawer>
-        </Box>
+        </>
     );
 }
