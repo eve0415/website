ad7e4ba2aea81a0df05eeccc8c30e05f1644a53c
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-19 12:52:55 +0900

    Revert "A few more"
    This reverts commit 630d9ac67d57c8e6d4fe4dc1a13d0383ac19c04d.

--- components/BackImage.tsx
+++ components/BackImage.tsx
@@ -3,24 +3,24 @@ import cat from '../public/images/cat.jpg';
 import catSleeping from '../public/images/catSleeping.jpg';
 import { CloudflareImage } from './CloudflareLoader';
 
-const BackImage = () => (
-    <Box
-        position='fixed'
-        width='100vw'
-        height='100vh'
-        sx={{
-            filter: { xs: 'blur(3px)', md: 'blur(5px)' },
-        }}
-    >
-        <CloudflareImage
-            src={useMediaQuery(useTheme().breakpoints.up('sm')) ? cat : catSleeping}
-            alt='My Cat'
-            layout='fill'
-            placeholder='blur'
-            objectFit='cover'
-            priority
-        />
-    </Box>
-);
-
-export default BackImage;
+export default function BackImage() {
+    return (
+        <Box
+            position='fixed'
+            width='100vw'
+            height='100vh'
+            sx={{
+                filter: { xs: 'blur(3px)', md: 'blur(5px)' },
+            }}
+        >
+            <CloudflareImage
+                src={useMediaQuery(useTheme().breakpoints.up('sm')) ? cat : catSleeping}
+                alt='My Cat'
+                layout='fill'
+                placeholder='blur'
+                objectFit='cover'
+                priority
+            />
+        </Box>
+    );
+}
--- components/Home.tsx
+++ components/Home.tsx
@@ -2,43 +2,43 @@ import { Box, Typography, useTheme } from '@mui/material';
 import profilePic from '../public/images/me.png';
 import { CloudflareImage } from './CloudflareLoader';
 
-const Home = () => (
-    <Box
-        display='flex'
-        justifyContent='center'
-        flexDirection='column'
-        alignItems='center'
-        position='relative'
-        height={`calc(100vh - ${useTheme().spacing(10)})`}
-        pb={{ xs: '20%', md: 0 }}
-        color='white'
-    >
+export default function Home() {
+    return (
         <Box
-            width={{ xs: 100, md: 200, lg: 300 }}
-            height={{ xs: 100, md: 200, lg: 300 }}
-            position='sticky'
-            borderRadius='50%'
-            overflow='hidden'
+            display='flex'
+            justifyContent='center'
+            flexDirection='column'
+            alignItems='center'
+            position='relative'
+            height={`calc(100vh - ${useTheme().spacing(10)})`}
+            pb={{ xs: '20%', md: 0 }}
+            color='white'
         >
-            <CloudflareImage
-                src={profilePic}
-                alt='My profile picture'
-                layout='fill'
-                placeholder='blur'
-                sizes='(min-width: 1200px) 300px, (min-width: 900px) 200px, 100px'
-                priority
-            />
+            <Box
+                width={{ xs: 100, md: 200, lg: 300 }}
+                height={{ xs: 100, md: 200, lg: 300 }}
+                position='sticky'
+                borderRadius='50%'
+                overflow='hidden'
+            >
+                <CloudflareImage
+                    src={profilePic}
+                    alt='My profile picture'
+                    layout='fill'
+                    placeholder='blur'
+                    sizes='(min-width: 1200px) 300px, (min-width: 900px) 200px, 100px'
+                    priority
+                />
+            </Box>
+
+            <Typography variant='h3' pt={4}>
+                eve0415
+            </Typography>
+
+            <Typography variant='body1' pt={1}>
+                ただの大学生
+            </Typography>
+            <Typography variant='body1'>いつも眠たい人</Typography>
         </Box>
-
-        <Typography variant='h3' pt={4}>
-            eve0415
-        </Typography>
-
-        <Typography variant='body1' pt={1}>
-            ただの大学生
-        </Typography>
-        <Typography variant='body1'>いつも眠たい人</Typography>
-    </Box>
-);
-
-export default Home;
+    );
+}
