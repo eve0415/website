630d9ac67d57c8e6d4fe4dc1a13d0383ac19c04d
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-19 12:49:20 +0900

    A few more

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,43 +2,43 @@ import { Box, Typography, useTheme } from '@mui/material';
 import profilePic from '../public/images/me.png';
 import { CloudflareImage } from './CloudflareLoader';
 
-export default function Home() {
-    return (
+const Home = () => (
+    <Box
+        display='flex'
+        justifyContent='center'
+        flexDirection='column'
+        alignItems='center'
+        position='relative'
+        height={`calc(100vh - ${useTheme().spacing(10)})`}
+        pb={{ xs: '20%', md: 0 }}
+        color='white'
+    >
         <Box
-            display='flex'
-            justifyContent='center'
-            flexDirection='column'
-            alignItems='center'
-            position='relative'
-            height={`calc(100vh - ${useTheme().spacing(10)})`}
-            pb={{ xs: '20%', md: 0 }}
-            color='white'
+            width={{ xs: 100, md: 200, lg: 300 }}
+            height={{ xs: 100, md: 200, lg: 300 }}
+            position='sticky'
+            borderRadius='50%'
+            overflow='hidden'
         >
-            <Box
-                width={{ xs: 100, md: 200, lg: 300 }}
-                height={{ xs: 100, md: 200, lg: 300 }}
-                position='sticky'
-                borderRadius='50%'
-                overflow='hidden'
-            >
-                <CloudflareImage
-                    src={profilePic}
-                    alt='My profile picture'
-                    layout='fill'
-                    placeholder='blur'
-                    sizes='(min-width: 1200px) 300px, (min-width: 900px) 200px, 100px'
-                    priority
-                />
-            </Box>
-
-            <Typography variant='h3' pt={4}>
-                eve0415
-            </Typography>
-
-            <Typography variant='body1' pt={1}>
-                ただの大学生
-            </Typography>
-            <Typography variant='body1'>いつも眠たい人</Typography>
+            <CloudflareImage
+                src={profilePic}
+                alt='My profile picture'
+                layout='fill'
+                placeholder='blur'
+                sizes='(min-width: 1200px) 300px, (min-width: 900px) 200px, 100px'
+                priority
+            />
         </Box>
-    );
-}
+
+        <Typography variant='h3' pt={4}>
+            eve0415
+        </Typography>
+
+        <Typography variant='body1' pt={1}>
+            ただの大学生
+        </Typography>
+        <Typography variant='body1'>いつも眠たい人</Typography>
+    </Box>
+);
+
+export default Home;
