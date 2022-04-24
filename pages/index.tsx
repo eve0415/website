a00b8bac53fea5de5b8693703f81b61022c18743
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-04-24 23:36:49 +0900

    properly fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home = dynamic(() => import('../components/Home'), { ssr: false });
 
 export default function Head() {
     return (
-        <Box display='flow-root' height='100vh' overflow='auto'>
+        <Box display='flow-root'>
             <BackImage />
             <Box position='fixed' width='100vw' height='100vh' bgcolor='rgba(0,0,0,0.5)' />
 
