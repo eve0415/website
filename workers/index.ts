f8194ea0ad5e9079ca35ead0d16080a72eb37aeb
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-06-11 23:51:27 +0900

    use kv as cache

--- workers/index.ts
+++ workers/index.ts
@@ -3,6 +3,7 @@ import { etag } from 'hono/etag';
 
 interface Env {
     MY_BUCKET: R2Bucket;
+    cache: KVNamespace;
     AUTH_KEY_SECRET: string;
 }
 
@@ -20,27 +21,37 @@ app.use('*', async (ctx, next) => {
 });
 
 app.get('/:request', async ctx => {
-    console.log(ctx.env);
-    const object = await ctx.env.MY_BUCKET.get(ctx.req.param('request'));
+    const key = ctx.req.param('request');
+
+    const cache = await ctx.env.cache.get(key, 'arrayBuffer');
+    if (cache) return ctx.body(cache);
+
+    const object = await ctx.env.MY_BUCKET.get(key);
     if (!object?.body) return ctx.notFound();
 
+    ctx.event?.waitUntil(ctx.env.cache.put(key, await object.arrayBuffer()));
     return ctx.body(object.body);
 });
 
 app.put('/:request', async ctx => {
+    const key = ctx.req.param('request');
+
     if (!ctx.req.headers.get('Content-Type')?.includes(`multipart/form-data`)) {
-        await ctx.env.MY_BUCKET.put(ctx.req.param('request'), await ctx.req.text());
-        return new Response(`Put ${ctx.req.param('request')} successfully!`);
+        await ctx.env.MY_BUCKET.put(key, await ctx.req.text());
+        return new Response(`Put ${key} successfully!`);
     }
 
     const form = await ctx.req.formData();
     const file = form.get('file') as File;
-    await ctx.env.MY_BUCKET.put(ctx.req.param('request'), await file.arrayBuffer(), {
+    await ctx.env.MY_BUCKET.put(key, await file.arrayBuffer(), {
         httpMetadata: { contentType: file.type },
     });
 });
+
 app.delete('/:request', async ctx => {
     await ctx.env.MY_BUCKET.delete(ctx.req.param('request'));
+    ctx.event?.waitUntil(ctx.env.cache.delete(ctx.req.param('request')));
+
     return new Response('Deleted!', { status: 200 });
 });
 
