08968a338727b0b5611ed6db6cb1e4a661e49378
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-09-01 09:25:31 +0900

    fix: fix another broken things (#41)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, NoSsr } from '@mui/material';
 import BackImage from '../components/BackImage';
 import { Main } from '../components/Content';
 import Home from '../components/Home';
@@ -6,7 +6,9 @@ import Home from '../components/Home';
 export default function Head() {
     return (
         <Box display='flow-root'>
-            <BackImage />
+            <NoSsr>
+                <BackImage />
+            </NoSsr>
             <Box position='fixed' width='100vw' height='100vh' bgcolor='rgba(0,0,0,0.5)' />
 
             <Main>
