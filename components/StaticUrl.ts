9c01e005be788cc7a9e0317c49aa0633ec34ad60
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-09-17 21:35:25 +0900

    fix(image): change URL

--- components/StaticUrl.ts
+++ components/StaticUrl.ts
@@ -1,3 +1,3 @@
-export const profilePicture = 'https://website.eve0415.workers.dev/me.jpg';
+export const profilePicture = 'https://website.eve0415.workers.dev/me.webp';
 export const verticalPicture = 'https://website.eve0415.workers.dev/cat.jpg';
 export const horizontalPicture = 'https://website.eve0415.workers.dev/catSleeping.jpg';
