0be1f47a1f209faf8679c6034a2803355035f524
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-09-17 21:35:41 +0900

    refactor: remove unused code

--- components/Blob2Base64.ts
+++
@@ -1,9 +0,0 @@
-export const convertBlobToBase64 = (blob: Blob): Promise<string | ArrayBuffer | null> =>
-    new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onerror = reject;
-        reader.onload = () => {
-            resolve(reader.result);
-        };
-        reader.readAsDataURL(blob);
-    });
