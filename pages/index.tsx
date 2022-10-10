7a615751ec6eedf76a86fc95e00084c12a644607
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-10-10 11:44:22 +0900

    feat: add preconnect header (#50)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import { Box, NoSsr } from '@mui/material';
 import BackImage from '../components/BackImage';
 import { Main } from '../components/Content';
+import { PreConnect } from '../components/CustomImageProxy';
 import Home from '../components/Home';
 
 export default function Head() {
     return (
-        <Box display='flow-root'>
-            <NoSsr>
-                <BackImage />
-                <Box position='fixed' width='100vw' height='100vh' bgcolor='rgba(0,0,0,0.5)' />
+        <>
+            <PreConnect />
+            <Box display='flow-root'>
+                <NoSsr>
+                    <BackImage />
+                    <Box position='fixed' width='100vw' height='100vh' bgcolor='rgba(0,0,0,0.5)' />
 
-                <Main>
-                    <Home />
-                </Main>
-            </NoSsr>
-        </Box>
+                    <Main>
+                        <Home />
+                    </Main>
+                </NoSsr>
+            </Box>
+        </>
     );
 }
