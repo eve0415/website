d99ec6ff026348a34056c578a20cd7202764efdb
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-11-04 13:18:41 +0900

    feat: use @next/font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,6 @@ export default class WebDocument extends Document {
         return (
             <Html lang='ja'>
                 <Head>
-                    <link
-                        rel='stylesheet'
-                        href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&family=Noto+Sans+JP:wght@300;400;500;700&display=swap'
-                    />
                     <link rel='apple-touch-icon' type='image/png' href='/apple-touch-icon-180x180.png' />
                     <link rel='icon' type='image/png' href='/icon-192x192.png' />
                     <link rel='icon' type='image/png' href='/icon-256x256.png' />
