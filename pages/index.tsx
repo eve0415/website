2f3a0ce6274e7d6d835b8b4f9c80de7d7a1935cf
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-12-07 23:17:21 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,46 @@
-import { Box, NoSsr } from '@mui/material';
-import BackImage from '../components/BackImage';
+import { Box, Flex, MediaQuery, Overlay, Text, Title } from '@mantine/core';
 import { Main } from '../components/Content';
-import { PreConnect } from '../components/CustomImageProxy';
-import Home from '../components/Home';
+import { CustomImageProxy, PreConnect } from '../components/CustomImageProxy';
+import { horizontalPicture, profilePicture, verticalPicture } from '../components/StaticUrl';
 
 export default function Head() {
     return (
         <>
             <PreConnect />
-            <Box display='flow-root'>
-                <NoSsr>
-                    <BackImage />
-                    <Box position='fixed' width='100vw' height='100vh' bgcolor='rgba(0,0,0,0.5)' />
 
-                    <Main>
-                        <Home />
-                    </Main>
-                </NoSsr>
-            </Box>
+            <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+                <CustomImageProxy src={verticalPicture} alt='My Cat' fill />
+            </MediaQuery>
+            <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+                <CustomImageProxy src={horizontalPicture} alt='My Cat' fill />
+            </MediaQuery>
+
+            <Overlay zIndex={0} blur={5} sx={{ backgroundColor: 'rgba(0,0,0,0.4)' }} />
+
+            <Main>
+                <Flex direction='column' align='center' sx={{ color: 'white' }}>
+                    <Box
+                        w={{ base: 100, md: 200, lg: 300 }}
+                        h={{ base: 100, md: 200, lg: 300 }}
+                        sx={{ borderRadius: '50%', overflow: 'hidden' }}
+                        pos='relative'
+                    >
+                        <CustomImageProxy
+                            src={profilePicture}
+                            alt='My profile picture'
+                            // sizes='(min-width: 1200px) 300px, (min-width: 900px) 200px, 100px'
+                            fill
+                            // priority
+                        />
+                    </Box>
+
+                    <Title size='3rem' pt='32px'>
+                        eve0415
+                    </Title>
+                    <Text fz='xl'>ただの大学生</Text>
+                    <Text fz='xl'>いつも眠たい人</Text>
+                </Flex>
+            </Main>
         </>
     );
 }
