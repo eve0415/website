5343bfaa637b2b84b52c1690a116815017f8b500
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-12-09 22:32:29 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { MantineProvider } from '@mantine/core';
-import { createTheme, CssBaseline, responsiveFontSizes, styled, ThemeProvider } from '@mui/material';
 import { Roboto } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
@@ -9,11 +8,6 @@ import { Navbar } from '../components/navbar';
 
 import '../public/global.css';
 
-const Root = styled('main')({
-    minHeight: '100vh',
-    overflow: 'hidden',
-    backgroundColor: '#E5FCFB',
-});
 const fonts = Roboto({
     weight: ['300', '400', '500', '700'],
     subsets: ['latin'],
@@ -25,46 +19,44 @@ export default function Website({ Component, pageProps, router }: AppProps) {
     const [open, setOpen] = useState(false);
 
     return (
-        <MantineProvider withGlobalStyles withNormalizeCSS>
-            <ThemeProvider
-                theme={responsiveFontSizes(
-                    createTheme({
-                        typography: {
-                            fontFamily: fonts.style.fontFamily,
-                        },
-                        components: {
-                            MuiCssBaseline: {
-                                styleOverrides: `
-                                ::-webkit-scrollbar {
-                                    height: 10px;
-                                    width: 10px;
-                                },
-                                ::-webkit-scrollbar-thumb {
-                                    background-color: #c7dfde;
-                                    border-radius: 10px;
-                                }
-                            `,
-                            },
-                        },
-                    })
-                )}
-            >
-                <Head>
-                    <title>eve0415</title>
-                    <meta name='viewport' content='initial-scale=1, width=device-width' />
-                </Head>
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            withCSSVariables
+            theme={{
+                respectReducedMotion: true,
+                fontFamily: fonts.style.fontFamily,
+                headings: {
+                    fontFamily: fonts.style.fontFamily,
+                    fontWeight: 400,
+                },
+                globalStyles: () => ({
+                    body: {
+                        backgroundColor: '#E5FCFB',
+                        minHeight: '100dvh',
+                    },
+                    '::-webkit-scrollbar': {
+                        height: '10px',
+                        width: '10px',
+                    },
+                    '::-webkit-scrollbar-thumb': {
+                        backgroundColor: '#c7dfde',
+                        borderRadius: '10px',
+                    },
+                }),
+            }}
+        >
+            <Head>
+                <title>eve0415</title>
+                <meta name='viewport' content='initial-scale=1, width=device-width' />
+            </Head>
 
-                <CssBaseline enableColorScheme />
-
-                <Root sx={{ minHeight: '100dvh' }}>
-                    <SwitchTransition mode='out-in'>
-                        <CSSTransition key={router.pathname} classNames='page' timeout={300} unmountOnExit>
-                            <Component {...pageProps} />
-                        </CSSTransition>
-                    </SwitchTransition>
-                    <Navbar isOpen={open} open={() => setOpen(true)} close={() => setOpen(false)} />
-                </Root>
-            </ThemeProvider>
+            <SwitchTransition mode='out-in'>
+                <CSSTransition key={router.pathname} classNames='page' timeout={300} unmountOnExit>
+                    <Component {...pageProps} />
+                </CSSTransition>
+            </SwitchTransition>
+            <Navbar isOpen={open} open={() => setOpen(true)} close={() => setOpen(false)} />
         </MantineProvider>
     );
 }
