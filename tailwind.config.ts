8e6db7ecb8d8a29605e488dadb157235eedbc562
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-06-22 16:19:25 +0900

    chore: update configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/*.{jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+} satisfies Config;
