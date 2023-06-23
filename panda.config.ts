5ac15649a8a7205e3f991de45bcae4d18a88e17a
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-06-23 14:53:28 +0900

    chore: configure panda

--- panda.config.ts
+++ panda.config.ts
@@ -1,21 +1,7 @@
-import { defineConfig } from "@pandacss/dev"
+import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
-    // Whether to use css reset
-    preflight: true,
-    
-    // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
-
-    // Files to exclude
-    exclude: [],
-
-    // Useful for theme customization
-    theme: {
-      extend: {}
-    },
-
-    // The output directory for your css system
-    outdir: "styled-system",
-    
-})
+  preflight: true,
+  include: ["./app/**/*.{js,jsx,ts,tsx}"],
+  outdir: "styled-system",
+});
