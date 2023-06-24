360fdfca57db53e04473e48f4a3c171ee23d3674
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-06-24 13:46:34 +0900

    fix: navbar

---
+++ components/NavBar/Item.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Route } from "next";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FC } from "react";
+import { css } from "styled-system/css";
+import { flex } from "styled-system/patterns";
+
+const Item: FC<{
+  page: { name: string; icon: JSX.Element; path: string };
+}> = ({ page: { name, icon, path } }) => {
+  const pathName = usePathname();
+
+  return (
+    <Link
+      href={path as Route}
+      className={flex({
+        paddingX: 16,
+        paddingY: 4,
+        marginY: 2,
+        alignItems: "center",
+        width: "100%",
+        bgColor: pathName === path ? "#E5FCFB" : "white",
+      })}
+    >
+      {icon}
+      <h3 className={css({ paddingLeft: 2 })}>{name}</h3>
+    </Link>
+  );
+};
+
+export default Item;
