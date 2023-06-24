59e83aa169a8dbbc57ed87ecd67e25b6f090f81a
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-06-25 01:25:12 +0900

    refactor: add background image

--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,39 @@ import { circle, flex } from 'styled-system/patterns';
 
 export default function Page() {
   return (
-    <div
-      className={flex({
-        direction: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100dvh',
-      })}
-    >
+    <>
+      <Image
+        src='https://eve0415.net/cdn-cgi/imagedelivery/e1FmkEoJCgY0rL0NK8GfGA/1eec637e-fed1-46b7-6c35-bcf496fe2c01/background'
+        alt='background'
+        style={{ objectFit: 'cover' }}
+        fill
+      />
+
       <div
-        className={circle({
-          size: 300,
-          position: 'relative',
+        className={flex({
+          direction: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100dvh',
         })}
       >
-        <Image
-          src='https://eve0415.net/cdn-cgi/imagedelivery/e1FmkEoJCgY0rL0NK8GfGA/648ac891-edcf-4ae6-2c20-9cc7adae0401/public'
-          alt='me'
-          className={css({ borderRadius: '50%' })}
-          fill
-        />
-      </div>
+        <div
+          className={circle({
+            size: { mdDown: 200, md: 300 },
+            position: 'relative',
+          })}
+        >
+          <Image
+            src='https://eve0415.net/cdn-cgi/imagedelivery/e1FmkEoJCgY0rL0NK8GfGA/648ac891-edcf-4ae6-2c20-9cc7adae0401/avatar'
+            alt='me'
+            className={css({ borderRadius: '50%' })}
+            fill
+          />
+        </div>
 
-      <h1 className={css({ fontSize: '5xl' })}>eve0415</h1>
-      <p className={css({ fontSize: 'lg', fontSizeAdjust: {} })}>学生というステータスを失った人</p>
-    </div>
+        <h1 className={css({ fontSize: '5xl' })}>eve0415</h1>
+        <p className={css({ fontSize: 'lg' })}>学生というステータスを失った人</p>
+      </div>
+    </>
   );
 }
