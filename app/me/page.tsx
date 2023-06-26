72db1e734ed54ab1b795c4e68b2af3a6a6da97f4
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-06-26 10:28:27 +0900

    refactor: another one

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -4,13 +4,11 @@ import { title } from './styles';
 
 export default function Page() {
   return (
-    <>
-      <div className={paper()}>
-        <h2 className={title}>自己紹介</h2>
-        <p>猫と寝るの大好き</p>
-        <p>毎日プログラム書いてます</p>
-        <p>日本人ですが、どちらかといえば英語の方が得意です</p>
-      </div>
-    </>
+    <div className={paper()}>
+      <h2 className={title}>自己紹介</h2>
+      <p>猫と寝るの大好き</p>
+      <p>毎日プログラム書いてます</p>
+      <p>日本人ですが、どちらかといえば英語の方が得意です</p>
+    </div>
   );
 }
