53ae9be3533150bf75dba7ce490e3410dc9c2de4
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-06-28 07:14:44 +0900

    refactor: see you

--- app/projects/@simple/page.tsx
+++
@@ -1,7 +0,0 @@
-import Card from './Card';
-import { bots } from './bot';
-import { minecraft } from './minecraft';
-
-export default function Page() {
-  return [...bots, ...minecraft].map(data => <Card key={data.name} data={data} />);
-}
