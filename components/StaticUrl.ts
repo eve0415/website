154b31fc8b756e891dc092f869cde556783a793d
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-06-28 07:25:15 +0900

    refactor: see you

--- components/StaticUrl.ts
+++
@@ -1,3 +0,0 @@
-export const profilePicture = 'https://website-r2.eve0415.net/me.webp';
-export const verticalPicture = 'https://website-r2.eve0415.net/cat.jpg';
-export const horizontalPicture = 'https://website-r2.eve0415.net/catSleeping.jpg';
--- components/me/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './editor';
-export * from './Interface';
-export * from './notUsed';
-export * from './technology';
