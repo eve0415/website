f656d4a1d7bf8b7a75576e9a0ab0f3e30e5ec4bc
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-11-27 10:18:43 +0900

    feat: introduce new font (#99)
    https://monaspace.githubnext.com/

--- app/(general)/me/@language/page.tsx
+++ app/(general)/me/@language/page.tsx
@@ -1,3 +1,5 @@
+import localFont from 'next/font/local';
+
 import { paper } from 'styled-system/recipes';
 
 import Card from '../Card';
@@ -5,12 +7,18 @@ import { gridClass, title } from '../styles';
 
 import { languages } from './language';
 
+const neon = localFont({
+  src: '../../../../assets/MonaspaceNeon-Regular.woff',
+  display: 'swap',
+  variable: '--font-neon',
+});
+
 export default function Page() {
   return (
     <div className={paper()}>
       <h2 className={title}>プログラミング言語</h2>
 
-      <div className={gridClass}>
+      <div className={`${neon.className} ${gridClass}`}>
         {languages.map(language => (
           <Card key={language.name} data={language} />
         ))}
