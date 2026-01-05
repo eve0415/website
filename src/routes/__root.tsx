a66532a319ac6d6d946d072ee71ab7a52c527264
eve0415 <40632436+eve0415@users.noreply.github.com>
2026-01-05 18:39:36 +0900

    chore: setup tanstack start

---
+++ src/routes/__root.tsx
@@ -0,0 +1,42 @@
+import type { FC, PropsWithChildren } from "react";
+import { HeadContent, Outlet, Scripts, createRootRoute } from "@tanstack/react-router";
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+const RootDocument: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <html lang="ja">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+};
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "TanStack Start Starter",
+      },
+    ],
+  }),
+  component: RootComponent,
+});
