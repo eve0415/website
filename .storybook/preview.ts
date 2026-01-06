e0c06b7e59c7413a8b07cc85c9a60432d2457366
eve0415 <40632436+eve0415@users.noreply.github.com>
2026-01-07 08:53:22 +0900

    test: add default viewport

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,16 @@
 import addonA11y from '@storybook/addon-a11y';
 import addonDocs from '@storybook/addon-docs';
 import { definePreview } from '@storybook/react-vite';
+import { INITIAL_VIEWPORTS } from 'storybook/viewport';
 
 import '#routes/__root.css';
 
 export default definePreview({
   addons: [addonA11y(), addonDocs()],
   parameters: {
+    viewport: {
+      options: INITIAL_VIEWPORTS,
+    },
     a11y: { test: 'todo' },
   },
 });
